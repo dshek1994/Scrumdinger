e2d6bb0dceb3f3cab02241abd259e24484509c58
Daniel S <dshekari@icloud.com>
2021-10-04 23:45:47 -0500

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Daniel Shekari on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
